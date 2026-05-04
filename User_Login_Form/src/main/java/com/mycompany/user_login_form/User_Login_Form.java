5e1d625e2351706dff70bf83e12ec10a306bc788
Takalani Mbaimbai <25034321@mvula.univen.ac.za>
2026-05-04 16:26:16 +0200

    initial commit

---
+++ User_Login_Form/src/main/java/com/mycompany/user_login_form/User_Login_Form.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.user_login_form;
+
+/**
+ *
+ * @author 25034321
+ */
+public class User_Login_Form {
+
+    public static void main(String[] args) {
+        new Login_Form().setVisible(true);
+    }
+}
